52465201aa2d8cabb431dba6a68fda74077b6ce2
Lmerveil <lmerveil@student.42.fr>
2025-03-12 11:17:18 +0100

    websockets tests

--- ft_transcendence/ft_transcendence/asgi.py
+++ ft_transcendence/ft_transcendence/asgi.py
@@ -1,9 +1,9 @@
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import onlinePong.routing
+from channels.auth import AuthMiddlewareStack
+import ft_transcendence.ft_transcendence.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ft_transcendence.settings')
 
@@ -13,7 +13,7 @@ application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            onlinePong.routing.websocket_urlpatterns
+            ft_transcendence.ft_transcendence.routing.websocket_urlpatterns
         )
     ),
 })
--- ft_transcendence/ft_transcendence/settings.py
+++ ft_transcendence/ft_transcendence/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'channels',
+    'onlinePong',
 ]
 
 AUTHENTICATION_BACKENDS = (
