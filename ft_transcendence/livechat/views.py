ef9bfcf3d230abe74414579211182fb456e7c62c
Lmerveil <lmerveil@student.42.fr>
2025-04-21 14:23:04 +0200

    removed empty livechat folder

--- ft_transcendence/livechat/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_protect
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-
-def livechat(request):
-    return render(request, 'livechat/livechat.html')
